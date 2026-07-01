35b752ee3ea324ce333c33ab659ae5e1f3f40f73
Namdev <Namdevkadam6266@gmail.com>
2026-07-01 09:09:32 +0530

    fix: remove trailing slash from CORS origin URL

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://lopy-snowy.vercel.app/",
+    origin:"https://lopy-snowy.vercel.app",
     credentials:true
 }))
 app.use('/user',userRouter);
